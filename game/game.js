e375918e990e47074811a307d96e7b35d00a3ed2
Omar Huseynov <omarhuseynov69@gmail.com>
2016-03-21 09:03:24 +0200

    Initial Commit

---
+++ game/game.js
@@ -0,0 +1,37 @@
+﻿/* NeoTrap developed by Omar */
+
+/// <reference path="../engine/Babylon.js" />
+/// <reference path="../game/scene.js" />
+/// <reference path="../sys/window.js" />
+/// <reference path="../debug/_debugMain.js" />
+
+/***********************************************/
+
+/* GLOBAL VARS */
+var canvas, engine, scene, cam, debugLayer, root;
+
+/* MAIN GAME */
+window.addEventListener("DOMContentLoaded", function () {
+
+    canvas = document.getElementById("renderCanvas");
+    engine = new BABYLON.Engine(canvas, true);
+
+    createScene(canvas, engine);
+
+    engine.enableOfflineSupport = true;
+
+    debugLayer = new BABYLON.DebugLayer(scene);
+
+    // GAME LOOP
+    engine.runRenderLoop(function () {
+        moveCamera(1);
+        scene.render();
+    });
+
+    // Resize Window
+    resizeWindow();
+});
+
+function moveCamera(speed) {
+    cam.position.z += speed;
+}
---
+++ game/scene.js
@@ -0,0 +1,46 @@
+﻿/* NeoTrap Scene development */
+
+/// <reference path="../engine/Babylon.js" />
+/// <reference path="../game/game.js" />
+
+function createScene() {
+
+    scene = new BABYLON.Scene(engine);
+
+    scene.fogEnabled = true;
+    scene.fogMode = BABYLON.Scene.FOGMODE_LINEAR;
+    scene.fogColor = scene.clearColor;
+    scene.fogEnd = 100;
+
+    scene.ambientColor = scene.clearColor;
+
+    createCamera();
+
+    createTunnel();
+}
+
+function createCamera() {
+    cam = new BABYLON.FreeCamera("Free Camera", new BABYLON.Vector3(0, 5, -75), scene);
+    cam.attachControl(canvas, false);
+}
+
+function createTunnel() {
+    // create a new node called "environment"
+    var envNode = new BABYLON.Node("Environment", scene);
+
+    // create tunnel meshes
+    var floor1 = BABYLON.Mesh.CreateGround("Floor1", 15, 150, 5, scene);
+    var floor2 = BABYLON.Mesh.CreateGround("Floor2", 15, 150, 5, scene);
+
+    // set positions and rotations
+    floor1.position = new BABYLON.Vector3.Zero();
+    floor2.position = new BABYLON.Vector3(0, 0, 155);
+
+    // create new emissive material
+    var mat = new BABYLON.StandardMaterial("Main Emissive", scene);
+    mat.emissiveColor = new BABYLON.Color3(255 / 255.0, 205 / 255.0, 210 / 255.0);
+
+    // apply the material
+    floor1.material = mat;
+    floor2.material = mat;
+}
